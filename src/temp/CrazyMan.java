af59c2a8d7c6767b0e45dee6878ac43e8e745311
Tax0787 <in_huan08@naver.com>
2023-08-04 21:36:47 +0900

    230804Desk

---
+++ src/temp/CrazyMan.java
@@ -0,0 +1,10 @@
+package temp;
+
+public interface CrazyMan {
+	
+	public String sayingSomething(int commentNumber);
+	public String whyCrazy();
+	public String hospital(int listNumber);
+	public String disease(int listNumber);
+
+}
---
+++ src/temp/Main.java
@@ -0,0 +1,16 @@
+package temp;
+
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		CrazyManImpl MeChinNom = new CrazyManImpl("미 친놈");
+		System.out.println(MeChinNom.getName());
+		System.out.println(MeChinNom.sayingSomething(1));
+		System.out.println(MeChinNom.whyCrazy());
+		System.out.println(MeChinNom.disease(1));
+		System.out.println(MeChinNom.hospital(1));
+		System.out.println("작성자도 정상이 아니혀조ㅕㅑ허ㅗㅕㅑㅈ겨려ㅐ2ㅈ갷ㅈ갸ㅐ허ㅐ");
+	}
+}
+
